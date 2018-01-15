49ef5a33ba770652d3a7e03a8fe5793173590951
amandinemeeus <amandine.meeus@alumni.ulb.be>
2018-01-15 12:07:35 +0100

    work in progress (bis)

--- include/header.php
+++ include/header.php
@@ -2,18 +2,27 @@
       <div class="container">
         <a class="navbar-brand" href="index.php">monsite.dev</a>
         <ul class="nav navbar-nav pull-xs-right text-xs-center">
-          <li class="nav-item">
-            <a class="nav-link" href="compte.php">Compte</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="deconnexion.php">Déconnexion</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="inscription.php">Inscription</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="connexion.php">Connexion</a>
-          </li>
+          
+            <?php if (isset($_SESSION['id'])) : ?>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="compte.php">Compte</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="deconnexion.php">Déconnexion</a>
+            </li>
+
+            <?php else : ?>
+
+            <li class="nav-item">
+                <a class="nav-link" href="inscription.php">Inscription</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="connexion.php">Connexion</a>
+            </li>
+
+            <?php endif; ?>
+
         </ul>
       </div>
     </nav>
